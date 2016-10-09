b373de192879e406a338fc257262d860f769d650
Ilya Murzinov <murz42@gmail.com>
2016-10-09 16:18:08 +0300

    Deployment script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout hakyll
+
+# Build new files
+stack build
+stack exec site clean
+stack exec site build
+
+# Get previous files
+git fetch -all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+rsync -a --filter='P _site/' --filter='P .git/' --filter='P .gitignore' --delete-excluded _site/ .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout hakyll
+git branch -D master
+git stash pop
